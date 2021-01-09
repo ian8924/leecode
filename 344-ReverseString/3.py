c53e8e7838b2a56348877e7756975bd7bf5e0b46
Ian Liao <justforian0915@gmail.com>
2021-01-09 21:37:05 +0800

    [add] 344-reverseString

--- 344-ReverseString/3.py
+++ 344-ReverseString/3.py
@@ -1,14 +1,10 @@
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        """
-        Do not return anything, modify s in-place instead.
-        """
-        i =int(0)
-        j= len(s)-1
-        while i<j:
-           print(i)
-           temp = s[i]
-           s[i] = s[j]
-           s[j] = temp
-           i+=1
-           j-=1
+s = ['h', 'e', 'l', 'l', '0']
+i = int(0)
+j = len(s) - 1
+while i < j:
+    print(i)
+    temp = s[i]
+    s[i] = s[j]
+    s[j] = temp
+    i += 1
+    j -= 1
